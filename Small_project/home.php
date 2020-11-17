b33819097dff731508e2e8cc22b6e31e196ddbeb
dieptuthinh <55689660+dieptuthinh@users.noreply.github.com>
2020-11-17 08:07:55 +0700

    fix

--- Small_project/home.php
+++ Small_project/home.php
@@ -1,34 +1,31 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <title>Document</title>
-</head>
-<body>
 <?php 
 session_start();
 
 if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
 
- ?>
+?>
 <!DOCTYPE html>
 <html>
 <head>
-	<title>HOME</title>
-	<link rel="stylesheet" type="text/css" href="css/style1.css">
+     <title>HOME</title>
+     <meta charset="UTF-8">
+     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+     <!-- <link rel="stylesheet" type="text/css" href="../INTERSHIP/Small_project/includes/style.css"> -->
 </head>
 <body>
-     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
-     <a href="logout.php">Logout</a>
-</body>
-</html>
-
+<?php include ('includes/header.html'); ?>
+     <div class="content">
+          <div class="container">
+               <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+               <a href="logout.php">Logout</a>
+          </div>
+     </div>
 <?php 
 }else{
      header("Location: login.php");
      exit();
 }
  ?>
+ <?php include ('includes/footer.html'); ?>
 </body>
 </html>
